05305ceb8c063ebad1bf43ef5c3d9f10f1a0a8e4
andrew.ryan <andrew.g.m.ryan@gmail.com>
2019-01-15 10:06:17 +0000

    prototype to write a simple grid cell membership function

--- hello-world/hello.pyx
+++ hello-world/hello.pyx
@@ -1,10 +1,30 @@
 from cython.parallel import prange
+import numpy as np
+cimport numpy as np
 
-cdef int i
-cdef int n = 30
-cdef int total = 0
 
-for i in prange(n, nogil=True):
-    total += i
+ctypedef np.uint8_t uint8
 
-print(total)
+
+def multiply_serial(double[:] x, double factor):
+    cdef Py_ssize_t i
+    for i in range(x.shape[0]):
+        x[i] = factor * x[i]
+
+
+def multiply_parallel(double[:] x, double factor):
+    cdef Py_ssize_t i
+    for i in prange(x.shape[0], nogil=True):
+        x[i] = factor * x[i]
+
+
+def grid_membership(double[:, :, :] corners):
+    cdef Py_ssize_t i
+    cdef uint8[:] inside = np.zeros(corners.shape[0], dtype=np.uint8)
+    for i in range(corners.shape[0]):
+        inside[i] = contains(corners[i])
+    return inside
+
+
+cdef uint8 contains(double[:, :] corners):
+    return True
